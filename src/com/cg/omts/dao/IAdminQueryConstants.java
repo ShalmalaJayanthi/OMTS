a54bf2f18e3e0b3d72f723bd0826864ef48f59b8
prasanna <a@laxmiprasanna>
2020-10-08 11:34:52 +0530

    seventh commit

--- src/com/cg/omts/dao/IAdminQueryConstants.java
+++ src/com/cg/omts/dao/IAdminQueryConstants.java
@@ -1,15 +1,11 @@
 package com.cg.omts.dao;
 
 public interface IAdminQueryConstants {
-<<<<<<< HEAD
+
 	
 	String GET_THEATRE_DETAILS = "select * from theatre";
 	
 	String ADD_THEATRE = "insert into theatre valules(?,?,?,?,?)";
-}
-=======
-	public static String GET_THEATRE_DETAILS= "SELECT * FROM THEATRE"; 
-	public static String ADD_THEATRE="INSERT INTO THEATRE VALUES(?,?,?,?,?)";
+	
 	public static String DELETE_THEATRE = "DELETE FROM THEATRE WHERE THEATREID=?";
 }
->>>>>>> branch 'master' of https://github.com/PLaxmiPrasanna/OnlineMovieTicketSystem
