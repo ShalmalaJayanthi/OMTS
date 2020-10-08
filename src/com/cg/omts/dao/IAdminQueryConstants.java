00f44cee40a8240a7afbf4fbf62b4a379e4a7535
vempalisupraja <vempalisupraja@gmail.com>
2020-10-08 10:48:57 +0530

    Adding Admin interface and DAO to master branch

--- src/com/cg/omts/dao/IAdminQueryConstants.java
+++ src/com/cg/omts/dao/IAdminQueryConstants.java
@@ -1,5 +1,7 @@
 package com.cg.omts.dao;
 
 public interface IAdminQueryConstants {
-
+	public static String GET_THEATRE_DETAILS= "SELECT * FROM THEATRE"; 
+	public static String ADD_THEATRE="INSERT INTO THEATRE VALUES(?,?,?,?,?)";
+	public static String DELETE_THEATRE = "DELETE FROM THEATRE WHERE THEATREID=?";
 }
